bd9485a4d1af280919f9bd5fca8d5cab27f86b1c
Jane Kennerly <janekennerly@gmail.com>
2022-09-28 22:25:05 -0700

    Added function introduction and examples

--- Lab1 - Intro to MATLAB/triangleArea.m
+++ Lab1 - Intro to MATLAB/triangleArea.m
@@ -1,9 +1,14 @@
-% Lab 1 - Intro to Matlab
+% TCES 455 Lab 1 - Intro to MATLAB
+% Written by: Jane Kennerly
 % Q3 - Calculate the area of a triangle given 3 sides
 
+% Creates a function called triangleArea which calculates the area
+% of a triangle given an array 'sides'
+% Ex 1: triangleArea([56, 27, 43]) 
+% Ex 2: triangleArea([5,12,13])
 function area = triangleArea(sides)
     s = 0;
-    for i = 1:length(sides)
+    for i = 1:3
         s = s + sides(i);
     end
     s = s/2;
