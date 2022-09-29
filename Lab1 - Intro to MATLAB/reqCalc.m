6939703bc62a0b42a76f0383eb4e6a5542d162a1
Jane Kennerly <janekennerly@gmail.com>
2022-09-28 22:01:31 -0700

    Corrected answer output. Completed documentation.

--- Lab1 - Intro to MATLAB/reqCalc.m
+++ Lab1 - Intro to MATLAB/reqCalc.m
@@ -1,8 +1,20 @@
+% TCES 455 Lab 1 - Intro to MATLAB
+% Written by: Jane Kennerly
 % Q2 - R_EQ of n parallel resistors
+
+% Creates a function called reqCalc which calculates equivalent resistance
+% (req) for n number of parallel resistors
+
+% parameter 'resistances' is expected to be an array
+
+% Ex: if the parallel resistor values are 10 ohms, 20 ohms, and 30 ohms,
+% call the function in the command window like this:
+% reqCalc([10,20,30])
 
 function req = reqCalc(resistances)
     req = 0;
     for i = 1:length(resistances)
         req = req + (1/resistances(i));
     end
+    req = 1/req;
 end
